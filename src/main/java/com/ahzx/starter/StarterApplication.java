3347ad6c2985f9932569a00ba37848691210263e
austgh <46312526+austgh@users.noreply.github.com>
2021-09-03 20:34:04 +0800

    2021-09-03

---
+++ src/main/java/com/ahzx/starter/StarterApplication.java
@@ -0,0 +1,16 @@
+package com.ahzx.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author think
+ */
+@SpringBootApplication
+public class StarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarterApplication.class, args);
+    }
+
+}
